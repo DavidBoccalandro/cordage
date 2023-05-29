eb5cc07b2b0a203dc54fa328cb8d6ff802162ade
DavidBoccalandro <davidboccalandro@hotmail.com>
2023-05-29 12:20:47 -0300

    :goal_net: add some basic catch errors

--- src/services/axios/axios.ts
+++ src/services/axios/axios.ts
@@ -1,3 +1,11 @@
 import axios from 'axios';
 
-export const dataGlossary = axios.get('../../../server/dataGlossaryTT.json');
+export const getDataGlossary = async () => {
+  try {
+    const response = await axios.get('../../../server/dataGlossaryTT.json');
+    return response.data;
+  } catch (error) {
+    console.error("Error getting glossary data:", error);
+    return null;
+  }
+};
