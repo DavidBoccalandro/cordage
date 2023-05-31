102955c9163d07d9e8d15b445a66bc899e6b8521
DavidBoccalandro <davidboccalandro@hotmail.com>
2023-05-31 01:05:17 -0300

    :sparkles: add reusable divider component

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const DividerContainer = styled.hr`
+  border: 1px solid var(--neutral200);
+  width: 100%;
+  margin: 24px 0;
+`;
+
+export const Divider = () => {
+  return (
+    <DividerContainer />
+  );
+};
