6237501c077d54d565abdaa294c99c19a7c71f0a
DavidBoccalandro <davidboccalandro@hotmail.com>
2023-07-11 11:30:08 -0300

    :rocket: update deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/cordage/'
-  }
-
-  return config
-})
+export default defineConfig({
+	plugins: [react()],
+	base: '/cordage/',
+});
